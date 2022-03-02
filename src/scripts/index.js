396cb474a141983c0787c2aaf8a922e4b6ec07d5
irwang-10 <irwangumilar111@gmail.com>
2022-03-02 08:51:22 +0700

    menambah sass-loader

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
 import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import '../styles/css/main.css';
+import '../styles/main.scss';
 
 console.log('Hello Coders! :)');
