3c6955c0530dadcdff693adad08d5ffa6105cfdb
irwang-10 <irwangumilar111@gmail.com>
2022-03-02 17:55:53 +0700

    update bad

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,15 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/css/main.css';
 import '../styles/main.scss';
 
-console.log('Hello Coders! :)');
+const hamburgerButtonElement = document.querySelector("#menu");
+const navListElement = document.querySelector(".nav-list");
+const mainElement = document.querySelector("main");
+
+hamburgerButtonElement.addEventListener("click", event => {
+    navListElement.classList.toggle("open");
+    event.stopPropagation();
+})
+mainElement.addEventListener("click", event => {
+    navListElement.classList.remove("open");
+    event.stopPropagation();
+})
