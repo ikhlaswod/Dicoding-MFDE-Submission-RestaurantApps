d796bed430efe505f6aaa788bdd33a59aaa9ee84
irwang-10 <irwangumilar111@gmail.com>
2022-03-02 23:52:39 +0700

    Update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/css/main.css';
 import '../styles/main.scss';
+// import ''
 
 const hamburgerButtonElement = document.querySelector("#menu");
 const navListElement = document.querySelector(".nav-list");
