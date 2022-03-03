b58482b57dfc028eb8dce96743d38f29cdeab334
irwang-10 <irwangumilar111@gmail.com>
2022-03-03 20:07:14 +0700

    update dengan json final project

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,17 @@
-import 'regenerator-runtime'; /* for async await transpile */
+import 'regenerator-runtime';
 import '../styles/css/main.css';
 import '../styles/main.scss';
-// import ''
+
+import data from '../data/DATA.json';
 
 const hamburgerButtonElement = document.querySelector("#menu");
 const navListElement = document.querySelector(".nav-list");
 const mainElement = document.querySelector("main");
+
+
+const restaurantListElement = document.querySelector("#restaurantList")
+
+
 
 hamburgerButtonElement.addEventListener("click", event => {
     navListElement.classList.toggle("open");
@@ -14,4 +20,27 @@ hamburgerButtonElement.addEventListener("click", event => {
 mainElement.addEventListener("click", event => {
     navListElement.classList.remove("open");
     event.stopPropagation();
+})
+document.addEventListener("DOMContentLoaded", () => {
+    restaurantListElement.innerHTML = "";
+    data.restaurants.forEach(restaurant => {
+        restaurantListElement.innerHTML += `
+        
+        <div class="list-item">
+            <div class="list-head">
+                <figure>
+                    <figcaption>${restaurant.city}</figcaption>
+                    <img src="${restaurant.pictureId}" alt="">
+                </figure>
+            </div>
+            <div class="list-text">
+                <h4>Rating : ${restaurant.rating}</h4>
+                <h2>${restaurant.name}</h2>
+                <p>${restaurant.description}</p>
+            </div>
+        </div>
+        `;
+    })
+
+
 })
