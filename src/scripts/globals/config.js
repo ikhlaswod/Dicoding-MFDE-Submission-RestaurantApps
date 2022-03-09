9ceab6d938a8c51fdd52f89a093421874b352acb
irwang-10 <irwangumilar111@gmail.com>
2022-03-09 23:59:09 +0700

    implementasi Cache API

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/large/',
+  CACHE_NAME: 'MovieCatalogue-V1',
 
 };
 export default CONFIG;
