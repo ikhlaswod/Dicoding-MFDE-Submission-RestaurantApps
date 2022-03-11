44f0c0df4834c11acfefe9a16a870507e5b22515
irwang-10 <irwangumilar111@gmail.com>
2022-03-11 21:00:35 +0700

    menyelesaikan get api detail restaurant

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,7 +1,12 @@
 import ApiRestaurantSource from '../../data/api-restaurant-source';
 import UrlParser from '../../routes/url-parser';
 import LikeButtonInitiator from '../../utils/like-button-initiator';
-import { createDetailRestaurantTemplate, createMenuTemplate } from '../template/template-creator';
+import {
+  createDetailRestaurantTemplate,
+  createMenuFoodTemplate,
+  createMenuDrinkTemplate,
+  createCategoryTemplate,
+} from '../template/template-creator';
 
 const Detail = {
   async render() {
@@ -20,9 +25,22 @@ const Detail = {
     const restaurant = await ApiRestaurantSource.detailRestaurant(url.id);
     const restaurantContainer = document.querySelector('.restaurant');
     restaurantContainer.innerHTML = createDetailRestaurantTemplate(restaurant.restaurant);
-    const menuContainer = document.querySelector('.menu-item');
-    // restaurant.restaurant.menus.forEach((menu) => )
-    menuContainer.innerHTML = createMenuTemplate(restaurant.restaurant.menus);
+    const foodContainer = document.querySelector('.foods-item');
+    const drinkContainer = document.querySelector('.drinks-item');
+    const categoryContainer = document.querySelector('.categories-item');
+
+    const { foods, drinks } = restaurant.restaurant.menus;
+    const { categories } = restaurant.restaurant;
+    foods.forEach((item) => {
+      foodContainer.innerHTML += createMenuFoodTemplate(item);
+    });
+
+    drinks.forEach((item) => {
+      drinkContainer.innerHTML += createMenuDrinkTemplate(item);
+    });
+    categories.forEach((category) => {
+      categoryContainer.innerHTML += createCategoryTemplate(category);
+    });
 
     LikeButtonInitiator.init({
       likeButtonContainer: document.querySelector('#likeButtonContainer'),
--- src/scripts/views/template/template-creator.js
+++ src/scripts/views/template/template-creator.js
@@ -18,51 +18,64 @@ const createListRestaurantTemplate = (restaurant) => `
 
 const createDetailRestaurantTemplate = (restaurant) => `
 <div class="detail-item">
-  <div class="detail-poster">
-    <img src="${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}" alt="">
-  </div>
-  <div class="detail-info">
-    <div class="restaurant-information">
-      <h1>Information</h1>
-      <hr style="height:2px;border-width:0;color:black;background-color:black">
-      <div class="card">
-        <h2>${restaurant.name}</h2>
-        <hr>
-        <p>${restaurant.description}</p>
-        </div>
-        <div class="info-category card">
-            <h3>Category</h3>
-            <hr>
-            <p>Italia, Modern</p>
-        </div>
-        <div class="info-menu card">
-            <h3>Menu</h3>
-            <hr>
-            <div class="menu-item">
-              
+    <div class="detail-poster">
+        <img src="${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}" alt="">
+    </div>
+    <div class="detail-info">
+        <div class="restaurant-information">
+            <h1>Information</h1>
+            <hr style="height:2px;border-width:0;color:black;background-color:black">
+            <div class="card">
+                <h2>${restaurant.name}</h2>
+                <hr>
+                <p>${restaurant.description}</p>
+            </div>
+            <div class="info-category card">
+                <h3>Category</h3>
+                <hr>
+                <p class="categories-item"></p>
+            </div>
+            <div class="info-menu card">
+                <h3>Menu</h3>
+                <hr>
+                <div class="menu-item">
+                  <div class="menu-food"> 
+                    <h4>Foods</h4>
+                    <hr class="hr-menu">
+                    <div class="foods-item">
+
+                    </div>
+                  </div>
+                  <div class="menu-drink">
+                    <h4>Drinks</h4>
+                    <hr class="hr-menu">
+                    <div class="drinks-item">
+
+                    </div>
+                  </div>
+                </div>
+            </div>
+            <div class="card">
+                <h3>Rating</h3>
+                <hr>
+                <p>${restaurant.rating}</p>
             </div>
         </div>
-        <div class="card">
-            <h3>Rating</h3>
-            <hr>
-            <p>${restaurant.rating}</p>
+        <div class="restaurant-review">
+            <h1>Reviews</h1>
+            <hr style="height:2px;border-width:0;color:black;background-color:black">
+            <div class="list-review">
+                <div class="review-item card">
+                    <h3>Irwan Gumilar</h3>
+                    <hr>
+                    <p>- Wih keren banget!!</p>
+                </div>
+            </div>
+            <div class="form-review card">
+                <textarea name="komentar" class="txtarea-style">Silahkan isi komentar anda</textarea>
+                <button type="submit" class="btn-submit">Submit</button>
+            </div>
         </div>
-      </div>
-      <div class="restaurant-review">
-          <h1>Reviews</h1>
-          <hr style="height:2px;border-width:0;color:black;background-color:black">
-          <div class="list-review">
-              <div class="review-item card">
-                  <h3>Irwan Gumilar</h3>
-                  <hr>
-                  <p>- Wih keren banget!!</p>
-              </div>
-          </div>
-          <div class="form-review card">
-              <textarea name="komentar" class="txtarea-style">Silahkan isi komentar anda</textarea>
-              <button type="submit" class="btn-submit">Submit</button>
-          </div>
-      </div>
     </div>
 </div>
 
@@ -80,17 +93,16 @@ const createDislikeButtonTemplate = () => `
   </button>
 `;
 
-const createMenuTemplate = (menus) => `
-  <div class="menu-food"> 
-    <h4>Foods</h4>
-    <hr class="hr-menu">
-    <p>${menus.foods}</p>
-    </div>
-    <div class="menu-drink">
-    <h4>Drinks</h4>
-    <hr class="hr-menu">
-    <p>${menus.drinks}</p>
-  </div>
+const createMenuFoodTemplate = (food) => `
+    <p>${food.name}</p>
+`;
+
+const createMenuDrinkTemplate = (drink) => ` 
+    <p>${drink.name}</p>
+`;
+
+const createCategoryTemplate = (categories) => `
+${categories.name} |
 `;
 
 export {
@@ -98,6 +110,8 @@ export {
   createDetailRestaurantTemplate,
   createLikeButtonTemplate,
   createDislikeButtonTemplate,
-  createMenuTemplate,
+  createMenuFoodTemplate,
+  createMenuDrinkTemplate,
+  createCategoryTemplate,
 
 };
