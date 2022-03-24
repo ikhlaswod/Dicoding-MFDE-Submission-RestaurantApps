04576e1a86eb071ca2a3e5be4862201148e74754
irwang-10 <irwangumilar111@gmail.com>
2022-03-24 18:46:10 +0700

    Memperbaiki dia yang terlupakan v2

--- specs/favoriteRestaurantSearchSpec.js
+++ specs/favoriteRestaurantSearchSpec.js
@@ -48,8 +48,8 @@ describe('Searching restaurants', () => {
     });
 
     it('should show the found restaurants by Favorite Restaurants', (done) => {
-      document.getElementById('restaurant-search-container')
-        .addEventListener('restaurants:searched:updated', () => {
+      document.getElementById('restaurants')
+        .addEventListener('restaurants:updated', () => {
           expect(document.querySelectorAll('.restaurant').length).toEqual(3);
           done();
         });
@@ -64,8 +64,8 @@ describe('Searching restaurants', () => {
     });
 
     it('should show the name of the restaurants found by Favorite Restaurants', (done) => {
-      document.getElementById('restaurant-search-container')
-        .addEventListener('restaurants:searched:updated', () => {
+      document.getElementById('restaurants')
+        .addEventListener('restaurants:updated', () => {
           const restaurantName = document.querySelectorAll('.restaurant__name');
           expect(restaurantName.item(0).textContent).toEqual('restaurant a');
           expect(restaurantName.item(1).textContent).toEqual('restaurant b');
@@ -84,8 +84,8 @@ describe('Searching restaurants', () => {
     });
 
     it('should show - when the movie returned does not contain a title', (done) => {
-      document.getElementById('restaurant-search-container')
-        .addEventListener('restaurants:searched:updated', () => {
+      document.getElementById('restaurants')
+        .addEventListener('restaurants:updated', () => {
           const restaurantNames = document.querySelectorAll('.restaurant__name');
           expect(restaurantNames.item(0).textContent).toEqual('-');
 
@@ -122,8 +122,8 @@ describe('Searching restaurants', () => {
 
   describe('When no favorite restaurant could be found', () => {
     it('should show the empty message', (done) => {
-      document.getElementById('restaurant-search-container')
-        .addEventListener('restaurants:searched:updated', () => {
+      document.getElementById('restaurants')
+        .addEventListener('restaurants:updated', () => {
           expect(document.querySelectorAll('.restaurants-item__not__found').length)
             .toEqual(1);
           done();
@@ -134,8 +134,8 @@ describe('Searching restaurants', () => {
       searchRestaurants('restaurant a');
     });
     it('should not show any movie', (done) => {
-      document.getElementById('restaurant-search-container')
-        .addEventListener('restaurants:searched:updated', () => {
+      document.getElementById('restaurants')
+        .addEventListener('restaurants:updated', () => {
           expect(document.querySelectorAll('.restaurant').length)
             .toEqual(0);
           done();
--- src/scripts/views/pages/liked-restaurants/favorite-restaurants-search-view.js
+++ src/scripts/views/pages/liked-restaurants/favorite-restaurants-search-view.js
@@ -2,61 +2,21 @@ import { createDetailRestaurantTemplate } from '../../template/template-creator'
 
 class FavoriteRestaurantSearchView {
   getTemplate() {
-    // return `
-    //   <div id="restaurant-search-container">
-    //       <input id="query" type="text">
-    //       <div class="restaurant-result-container">
-    //           <ul class="restaurants">
-    //           </ul>
-    //       </div>
-    //   </div>
-    //   `;
     return `
        <div class="content">
           <input id="query" type="text">
            <h2 class="content__heading">Your Liked Restaurant</h2>
-           <div id="restaurant-search-container">
-              <div id="restaurants" class="restaurants">
+            <div id="restaurants" class="restaurants">
 
-              </div>
-           </div>
+            </div>
        </div>
        `;
   }
-
-  // getFavoriteRestaurantTemplate() {
-  //   return `
-  //      <div class="content">
-  //          <h2 class="content__heading">Your Liked Restaurant</h2>
-  //          <div id="restaurants" class="restaurants">
-  //          </div>
-  //      </div>
-  //      `;
-  // }
 
   runWhenUserIsSearching(callback) {
     document.getElementById('query').addEventListener('change', (event) => {
       callback(event.target.value);
     });
-  }
-
-  showRestaurants(restaurants = []) {
-    let html;
-    if (restaurants.length > 0) {
-      html = restaurants.reduce(
-        (carry, restaurant) => carry.concat(createDetailRestaurantTemplate(restaurant)),
-        '',
-      );
-    } else {
-      html = this._getEmptyRestaurantTemplate();
-    }
-    document.querySelector('.restaurants')
-      .innerHTML = html;
-
-    document.getElementById('restaurant-search-container')
-      .dispatchEvent(new Event('restaurants:searched:updated'));
-
-    document.getElementById('restaurants').dispatchEvent(new Event('restaurants:updated'));
   }
 
   showFavoriteRestaurants(restaurants = []) {
