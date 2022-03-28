60bc9726aa42704b1ac897c09e2e20b0193ed77f
irwang-10 <irwangumilar111@gmail.com>
2022-03-28 10:37:24 +0700

    Jangan Sampai Salah Nama

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 Feature('Liking Restaurants');
 
 Before(({ I }) => {
@@ -9,17 +11,23 @@ Before(({ I }) => {
 //   I.see('Tidak ada restaurant yang dapat ditampilkan', '.restaurants-item__not__found');
 // });
 
-Scenario('liking one restaurants', ({ I }) => {
+Scenario('liking one restaurants', async ({ I }) => {
   I.see('Tidak ada restaurant yang dapat ditampilkan', '.restaurants-item__not__found');
 
   I.amOnPage('/');
 
   I.seeElement('.restaurant__name a');
-  I.click(locate('.restaurant__name a').first());
+
+  const firstRestaurant = locate('.restaurant__name a').first();
+  const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
 
   I.seeElement('#likeButton');
   I.click('#likeButton');
 
   I.amOnPage('/#/favorite');
   I.seeElement('.list-item');
+  const likedRestaurantName = await I.grabTextFrom('.restaurant__name');
+
+  assert.strictEqual(firstRestaurantName, likedRestaurantName);
 });
