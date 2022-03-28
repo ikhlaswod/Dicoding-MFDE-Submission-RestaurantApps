046d012e0f80f6cf117587e822687ac7fe52db17
irwang-10 <irwangumilar111@gmail.com>
2022-03-28 11:06:43 +0700

    Unliking restaurant

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -6,10 +6,10 @@ Before(({ I }) => {
   I.amOnPage('/#/favorite');
 });
 
-// Scenario('showing empty liked restaurants', ({ I }) => {
-//   I.seeElement('#query');
-//   I.see('Tidak ada restaurant yang dapat ditampilkan', '.restaurants-item__not__found');
-// });
+Scenario('showing empty liked restaurants', ({ I }) => {
+  I.seeElement('#query');
+  I.see('Tidak ada restaurant yang dapat ditampilkan', '.restaurants-item__not__found');
+});
 
 Scenario('liking one restaurants', async ({ I }) => {
   I.see('Tidak ada restaurant yang dapat ditampilkan', '.restaurants-item__not__found');
@@ -30,4 +30,27 @@ Scenario('liking one restaurants', async ({ I }) => {
   const likedRestaurantName = await I.grabTextFrom('.restaurant__name');
 
   assert.strictEqual(firstRestaurantName, likedRestaurantName);
+});
+
+Scenario('unliking one restaurant', async ({ I }) => {
+  I.see('Tidak ada restaurant yang dapat ditampilkan', '.restaurants-item__not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.restaurant__name a');
+
+  I.click(locate('.restaurant__name a').first());
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+  I.amOnPage('/');
+  I.seeElement('.list-item');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.restaurant__name a');
+  I.click(locate('.restaurant__name a').first());
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+  I.amOnPage('/#/favorite');
+
+  I.see('Tidak ada restaurant yang dapat ditampilkan', '.restaurants-item__not__found');
 });
