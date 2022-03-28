a3ee78098eace118d5602cb8605a9683e0172be5
irwang-10 <irwangumilar111@gmail.com>
2022-03-28 10:16:02 +0700

    Kenalkan, saya Codecept Konfigurasi codecept dan Menjalankan Testing Pertama

---
+++ e2e/Liking_Restaurants.spec.js
@@ -0,0 +1,10 @@
+Feature('Liking Restaurants');
+
+Before(({ I }) => {
+  I.amOnPage('/#/favorite');
+});
+
+Scenario('showing empty liked restaurants', ({ I }) => {
+  I.seeElement('#query');
+  I.see('Tidak ada restaurant yang dapat ditampilkan', '.restaurants-item__not__found');
+});
