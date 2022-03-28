2af923e0f890ada5b1e4586fb81d3504652fd85d
irwang-10 <irwangumilar111@gmail.com>
2022-03-28 10:24:20 +0700

    Membekukan waktu

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -4,7 +4,22 @@ Before(({ I }) => {
   I.amOnPage('/#/favorite');
 });
 
-Scenario('showing empty liked restaurants', ({ I }) => {
-  I.seeElement('#query');
+// Scenario('showing empty liked restaurants', ({ I }) => {
+//   I.seeElement('#query');
+//   I.see('Tidak ada restaurant yang dapat ditampilkan', '.restaurants-item__not__found');
+// });
+
+Scenario('liking one restaurants', ({ I }) => {
   I.see('Tidak ada restaurant yang dapat ditampilkan', '.restaurants-item__not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.restaurant__name a');
+  I.click(locate('.restaurant__name a').first());
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.list-item');
 });
